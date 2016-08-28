22ea9ad20115ae3a81ce1234825cc839be90cdaa
oshratCh <oshratc1994@gmail.com>
2016-08-28 19:05:48 +0300

    fix setup server
    * add node_modules folder
    * add suport in first request
    request: http://192.168.10.49:3000/api/v1/hello/
    response: "Hello I Vote App (;"

--- server.js
+++ server.js
@@ -1,86 +1,90 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+#!/usr/bin/env node
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/membit');
+/**
+ * Module dependencies.
+ */
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var reports = require('./routes/reports');
-var scores = require('./routes/scores');
-var dialogs = require('./routes/dialogs');
+var app = require('./main');
+var debug = require('debug')('IVoteAPI:server');
+var http = require('http');
 
+/**
+ * Get port from environment and store in Express.
+ */
 
-var app = express();
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+/**
+ * Create HTTP server.
+ */
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(app);
 
-app.use('/', routes);
-app.use('/api/v1/users/', users);
-app.use('/api/v1/scores/', scores);
-app.use('/api/v1/dialogs/', dialogs);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-// error handlers
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+/**
+ * Event listener for HTTP server "error" event.
+ */
 
-app.use(function (err,req,res,next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
-
-  // Pass to next layer of middleware
-  next();
-});
-module.exports = app;
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
